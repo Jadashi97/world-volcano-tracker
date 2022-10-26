9cad53fd85dd5f8b9d5ad27f135726972807be00
jadashi97 <njadauniversal23@yahoo.com>
2022-10-26 10:50:37 -0500

    (feat): initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './index.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="header">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+      <h1>World Volcano Tracker</h1>
+      <p>This is going to be a project to integrate the Nasa api to locate volcanoes around the world</p>
+    </div>
+  )
+}
